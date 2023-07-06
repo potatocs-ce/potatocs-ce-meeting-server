01650346eecd67e736c0fafe4d03d563fcda1017
park9379 <109051896+park9379@users.noreply.github.com>
2023-07-06 18:13:34 +0900

    file download api add

--- routes/apim/v1/user/user_index.js
+++ routes/apim/v1/user/user_index.js
@@ -4,7 +4,4 @@ const userController = require("./user_controller");
 /* 유저정보  */
 router.get("/profile", userController.profile);
 
-/* 유저정보  수정*/
-router.post("/profile", userController.profile);
-
 module.exports = router;
