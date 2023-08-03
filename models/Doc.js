1c825c7a319edb740502a028ed916e77fb35be23
park9379 <109051896+park9379@users.noreply.github.com>
2023-08-03 17:26:11 +0900

    change doc model schema

--- models/Doc.js
+++ models/Doc.js
@@ -1,56 +1,58 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+let docSchema = new Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    meetingId: {
+      type: String,
+    },
+    phoneNumber: {
+      type: String,
+    },
+    originalFileName: {
+      type: String,
+    },
+    fileName: {
+      type: String,
+    },
+    uploadUser: {
+      type: String,
+      // required: true, unique: true
+    },
+    saveKey: {
+      type: String,
+    },
+    fileSize: {
+      type: String,
+    },
+    drawingEventSet: [
+      {
+        _id: false,
+        pageNum: { type: Number },
+        drawingEvent: {
+          type: Object,
+          // point: [],
+          // timeDiff: { type: Number },
+          // tool: {
+          //   color: {
+          //     type: String
+          //   },
+          //   type: {
+          //     type: String
+          //   },
+          //   width: {
+          //     type: Number
+          //   },
+          // },
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    collection: "docs",
+  }
+);
 
-let docSchema = new Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  meetingId: {
-    type: String
-  },
-
-  originalFileName: {
-    type: String
-  },
-  fileName: {
-    type: String
-  },
-  uploadUser: {
-    type: String,
-    // required: true, unique: true 
-  },
-  saveKey: {
-    type: String
-  },
-  fileSize: {
-    type: String
-  },
-  drawingEventSet: [
-    {
-      _id: false,
-      pageNum: { type: Number },
-      drawingEvent: {
-        type: Object
-        // point: [],
-        // timeDiff: { type: Number },
-        // tool: {
-        //   color: {
-        //     type: String
-        //   },
-        //   type: {
-        //     type: String
-        //   },
-        //   width: {
-        //     type: Number
-        //   },
-        // },
-
-      }
-
-    }
-  ],
-}, {
-  timestamps: true,
-  collection: 'docs'
-})
-
-module.exports = mongoose.model('Doc', docSchema)
+module.exports = mongoose.model("Doc", docSchema);
