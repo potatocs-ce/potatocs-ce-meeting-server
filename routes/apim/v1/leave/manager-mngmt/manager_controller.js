ca1e28300fd280edab8c33b714ec3179ba4427e5
potatocs-ce <ce.potatocs@gmail.com>
2023-08-07 18:45:05 +0900

    Refertoring managerController update manager api

--- routes/apim/v1/leave/manager-mngmt/manager_controller.js
+++ routes/apim/v1/leave/manager-mngmt/manager_controller.js
@@ -2,62 +2,62 @@ const member = require('../../../../../models/member_schema');
 const manager = require('../../../../../models/manager_schema');
 
 exports.getManager = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
   User : ${req.decoded._id}
   API  : Get Manager Info
   router.get('/get-manager', managerMngmtCtrl.getManager);
   
 --------------------------------------------------`);
-	try {
-		const criteria = {
-			myId: req.decoded._id
-		}
+  try {
+    const criteria = {
+      myId: req.decoded._id
+    }
 
-		const projection = 'myManager myId accepted';
+    const projection = 'myManager myId accepted';
 
-		const requestedManager = await manager.findOne(criteria, projection);
-		// console.log('requested findManager', requestedManager);
+    const requestedManager = await manager.findOne(criteria, projection);
+    // console.log('requested findManager', requestedManager);
 
-		if(!requestedManager) {
-			return res.status(200).send({
-				message: 'findManager'
-			});
-		}
+    if (!requestedManager) {
+      return res.status(200).send({
+        message: 'findManager'
+      });
+    }
 
-		const managerCriteria = {
-			_id: requestedManager.myManager
-		};
+    const managerCriteria = {
+      _id: requestedManager.myManager
+    };
 
-		const managerProjection = 'email name isManager profile_img';
-		
-		const managerInfo = await member.findOne(managerCriteria, managerProjection);
-		// console.log('managerInfo', managerInfo);
+    const managerProjection = 'email name isManager profile_img';
 
-		const getManager = {
-			_id: requestedManager._id,
-			accepted: requestedManager.accepted,
-			email: managerInfo.email,
-			name: managerInfo.name,
-			profile_img: managerInfo.profile_img,
-		}
-		// console.log(returnData);
+    const managerInfo = await member.findOne(managerCriteria, managerProjection);
+    // console.log('managerInfo', managerInfo);
 
-		return res.status(200).send({
-			message: 'get Manager test',
-			getManager
-		})
+    const getManager = {
+      _id: requestedManager._id,
+      accepted: requestedManager.accepted,
+      email: managerInfo.email,
+      name: managerInfo.name,
+      profile_img: managerInfo.profile_img,
+    }
+    // console.log(returnData);
 
-	} catch (err) {
-		// console.log(err);
-		return res.status(500).send({
-			message: 'DB Error'
-		});
-	}
+    return res.status(200).send({
+      message: 'get Manager test',
+      getManager
+    })
+
+  } catch (err) {
+    // console.log(err);
+    return res.status(500).send({
+      message: 'DB Error'
+    });
+  }
 };
 
 exports.findManager = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
   User : ${req.decoded._id}
   API  : Find My Manager
@@ -66,36 +66,36 @@ exports.findManager = async (req, res) => {
   manager_email_id : ${req.params.id}
 --------------------------------------------------`);
 
-	try {
+  try {
 
-		const criteria = {
-			email: req.params.id
-		}
-	
-		const projection = 'email name profile_img mobile department';
+    const criteria = {
+      email: req.params.id
+    }
 
-		const user = await member.findOne(criteria, projection);
-		// console.log(user);
-		if(!user) {
-			return res.status(400).send({
-				message: 'Cannot find the manager'
-			});
-		}
+    const projection = 'email name profile_img mobile department';
 
-		return res.status(200).send({
-			user
-		});
+    const user = await member.findOne(criteria, projection);
+    // console.log(user);
+    if (!user) {
+      return res.status(400).send({
+        message: 'Cannot find the manager'
+      });
+    }
 
-	} catch (err) {
+    return res.status(200).send({
+      user
+    });
 
-		return res.status(500).send('DB Error');
+  } catch (err) {
 
-	}
-	
+    return res.status(500).send('DB Error');
+
+  }
+
 };
 
 exports.addManager = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
 	User : ${req.decoded._id}
 	API  : Add Manager
@@ -104,37 +104,37 @@ exports.addManager = async (req, res) => {
 	manager_id : ${req.body.manager_id}
 --------------------------------------------------`);
 
-	try {
-		
-		const newManager = manager({
-			myManager: req.body.manager_id,
-			myId: req.decoded._id,
-			accepted: false,
-			requestedDate: new Date()
-		});
-	
-		await newManager.save();
+  try {
 
-		res.send({
-			message: 'requested'
-		});
+    const newManager = manager({
+      myManager: req.body.manager_id,
+      myId: req.decoded._id,
+      accepted: false,
+      requestedDate: new Date()
+    });
+
+    await newManager.save();
+
+    res.send({
+      message: 'requested'
+    });
 
 
-	} catch (err) {
-		// console.log(err);
-		return res.status(500).send({
-			message: 'DB Error'
-		});
-	}
+  } catch (err) {
+    // console.log(err);
+    return res.status(500).send({
+      message: 'DB Error'
+    });
+  }
 
 };
 
 /*
-	manager_schema 안에 데이터가 있을 때
-	accepted => false = 펜딩중 true = 수락 후 매니저/직원 관계
+  manager_schema 안에 데이터가 있을 때
+  accepted => false = 펜딩중 true = 수락 후 매니저/직원 관계
 */
 exports.cancelPending = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
 	User : ${req.decoded._id}
 	API  : Cancel addManager Pending
@@ -143,22 +143,56 @@ exports.cancelPending = async (req, res) => {
 	manager_id : ${req.params.id}
 --------------------------------------------------`);
 
-	try {
+  try {
 
-		const criteria = {
-			_id: req.params.id
-		}
+    const criteria = {
+      _id: req.params.id
+    }
 
-		await manager.deleteOne(criteria);
+    await manager.deleteOne(criteria);
 
-		return res.status(200).send({
-			message: 'canceled'
-		});
+    return res.status(200).send({
+      message: 'canceled'
+    });
 
-	} catch (err) {
-		return res.status(500).send({
-			message: 'DB Error'
-		});
-	}
+  } catch (err) {
+    return res.status(500).send({
+      message: 'DB Error'
+    });
+  }
+
+};
+
+/*
+  manager_schema 안에 데이터가 있을 때
+  accepted => false = 펜딩중 true = 수락 후 매니저/직원 관계
+*/
+exports.updateManger = async (req, res) => {
+  console.log(`
+--------------------------------------------------
+	User : ${req.decoded._id}
+	API  : updateMangerager Pending
+	router.delete('/updateMangerg', managerMngmtCtrl.updateManger);
+
+	manager_id : ${req.params.id}
+--------------------------------------------------`);
+
+  try {
+
+    const criteria = {
+      _id: req.params.id
+    }
+
+    await manager.findByIdAndUpdate(criteria, { ...body });
+
+    return res.status(200).send({
+      message: 'canceled'
+    });
+
+  } catch (err) {
+    return res.status(500).send({
+      message: 'DB Error'
+    });
+  }
 
 };
