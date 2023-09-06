76135fd9bb5768e44c3974ad65eb83c075262569
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-09-06 18:33:37 +0900

    auth_index test api create

--- routes/apim/v1/auth/auth_index.js
+++ routes/apim/v1/auth/auth_index.js
@@ -3,8 +3,9 @@ const authController = require("./auth_controller");
 
 // /* 로그인  */
 router.post("/signIn", authController.signIn);
-
+router.post("/signInTest", authController.signInTest);
 // 로그아웃
 router.post("/signOut", authController.signOut);
+router.post("/signOutTest", authController.signOutTest);
 
 module.exports = router;
