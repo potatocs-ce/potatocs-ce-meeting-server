343be73f122bcd726c0a874c3ad13f861aa44605
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-09-13 17:58:21 +0900

    collab_index get api test complete

--- routes/apim/v1/collab/meeting/meeting_controller.js
+++ routes/apim/v1/collab/meeting/meeting_controller.js
@@ -144,38 +144,6 @@ exports.getChat = async (req, res) => {
     }
 };
 
-exports.getChatTest = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a chat
-  router.get('/getChat', MeetingContollder.getChat);
---------------------------------------------------`);
-    // console.log('[[getChat]] >>>>>> ', req.query.meetingId)
-
-    const dbModels = global.DB_MODELS;
-
-    try {
-        const criteria = {
-            meetingId: req.query.meetingId,
-        };
-
-        // 원하는 값만 query 하기 공백으로 구분
-        const MeetingChat = await dbModels.MeetingChat.find(criteria).select(
-            "userId chatMember createdAt chatContent"
-        );
-
-        if (!MeetingChat) {
-            return res.status(400).send("invalid meeting chat");
-        }
-
-        return res.status(200).send(MeetingChat);
-    } catch (err) {
-        return res.status(500).send({
-            message: "creatintg a meeting chat had an error",
-        });
-    }
-};
-
 /*
     Delete a chat
 */
@@ -298,90 +266,10 @@ exports.getParticipantState = async (req, res) => {
     }
 };
 
-exports.getParticipantStateTest = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a role
-  router.get('/getParticipantState', MeetingContollder.getParticipantState);
---------------------------------------------------`);
-    console.log("[[getParticipantState]] >>>>>> ", req.query.meetingId);
-
-    const dbModels = global.DB_MODELS;
-
-    try {
-        const criteria = {
-            _id: req.query.meetingId,
-        };
-
-        const currentMembers = await dbModels.Meeting.find(criteria).select(
-            "currentMembers"
-        );
-        // console.log('[[ getParticipantState ]]', currentMembers)
-        console.log("-------------------------------------------");
-
-        if (!currentMembers) {
-            return res.status(400).send("invalid meeting role");
-        }
-
-        return res.status(200).send(currentMembers);
-    } catch (err) {
-        return res.status(500).send({
-            message: "get a meeting role had an error",
-        });
-    }
-};
-
 /*
     Get a onLine
 */
 exports.getOnlineTrue = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a role
-  router.get('/getOnlineTrue', MeetingContollder.getOnlineTrue);
---------------------------------------------------`);
-    // console.log('[[getOnlineTrue]] >>>>>> ', req.query)
-
-    /**
-       * req.query = {
-       * 	 meetingId: '61dfc310f3aedb66a1786e8e',
-               userId: '61d3f9c13b83fffca344bf58'
-       * }
-       */
-    const dbModels = global.DB_MODELS;
-
-    try {
-        const getOnlineTrue = await dbModels.Meeting.findOneAndUpdate(
-            {
-                _id: req.query.meetingId, // meetingId
-                "currentMembers.member_id": req.query.userId, // userId
-            },
-            {
-                $set: {
-                    "currentMembers.$.online": true,
-                },
-            },
-            {
-                new: true,
-            }
-        );
-        // console.log('[[ getOnlineTrue ]]', getOnlineTrue)
-        console.log("-------------------------------------------");
-
-        if (!getOnlineTrue) {
-            return res.status(400).send("invalid meeting online");
-        }
-
-        return res.status(200).send(getOnlineTrue);
-    } catch (err) {
-        return res.status(500).send({
-            message: "get a meeting role had an error",
-        });
-    }
-};
-
-
-exports.getOnlineTrueTest = async (req, res) => {
     console.log(`
 --------------------------------------------------
   API  : Get a role
@@ -471,47 +359,6 @@ exports.getOnlineFalse = async (req, res) => {
     }
 };
 
-exports.getOnlineFalseTest = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a role
-  router.get('/getOnlineFalse', MeetingContollder.getOnlineFalse);
---------------------------------------------------`);
-    // console.log('[[getOnlineFalse]] >>>>>> ', req.query)
-
-    const dbModels = global.DB_MODELS;
-
-    try {
-        // meetingId를 이용하여 field 찾고 찾은 field에서 값 수정
-        // $는 배열의 몇 번째인지 index와 같은 역할
-        const getOnlineFalse = await dbModels.Meeting.findOneAndUpdate(
-            {
-                _id: req.query.meetingId, // meetingId
-                "currentMembers.member_id": req.query.userId, // userId
-            },
-            {
-                $set: {
-                    "currentMembers.$.online": false,
-                },
-            },
-            {
-                new: true,
-            }
-        );
-        // console.log('[[ getOnlineFalse ]]', getOnlineFalse)
-
-        if (!getOnlineFalse) {
-            return res.status(400).send("invalid meeting online");
-        }
-
-        return res.status(200).send(getOnlineFalse);
-    } catch (err) {
-        return res.status(500).send({
-            message: "get a meeting role had an error",
-        });
-    }
-};
-
 /*
     Update a Role
 */
@@ -556,91 +403,10 @@ exports.getRoleUpdate = async (req, res) => {
     }
 };
 
-exports.getRoleUpdateTest = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a role
-  router.get('/getRoleUpdate', MeetingContollder.getRoleUpdate);
---------------------------------------------------`);
-    console.log("[[getRoleUpdate]] >>>>>> ", req.query);
-
-    const dbModels = global.DB_MODELS;
-
-    try {
-        // meetingId를 이용하여 field 찾고 찾은 field에서 값 수정
-        // $는 배열의 몇 번째인지 index와 같은 역할
-        const getRoleUpdate = await dbModels.Meeting.findOneAndUpdate(
-            {
-                _id: req.query.meetingId, // meetingId
-                "currentMembers.member_id": req.query.userId, // userId
-            },
-            {
-                $set: {
-                    "currentMembers.$.role": req.query.role,
-                },
-            },
-            {
-                new: true,
-            }
-        );
-        // console.log('[[ getRoleUpdate ]]', getRoleUpdate)
-
-        if (!getRoleUpdate) {
-            return res.status(400).send("invalid meeting online");
-        }
-
-        return res.status(200).send(getRoleUpdate);
-    } catch (err) {
-        return res.status(500).send({
-            message: "get a meeting role had an error",
-        });
-    }
-};
-
 /*
     Update a meeting status
 */
 exports.getMeetingStatus = async (req, res) => {
-    console.log(`
---------------------------------------------------
-  API  : Get a meeting status
-  router.get('/getMeetingStatus', MeetingContollder.getMeetingStatus);
---------------------------------------------------`);
-    // console.log('[[getMeetingStatus]] >>>>>> ', req.query)
-
-    const dbModels = global.DB_MODELS;
-
-    try {
-        const criteria = {
-            _id: req.query.meetingId,
-        };
-
-        // meetingId를 이용하여 status 값 확인
-        const getMeetingStatus = await dbModels.Meeting.findOne(criteria).select(
-            "status"
-        );
-        // console.log('[[ getMeetingStatus ]]', getMeetingStatus)
-
-        if (!getMeetingStatus) {
-            return res.status(400).send("invalid getMeetingStatus");
-        }
-
-        const getStatus = {
-            _id: getMeetingStatus._id,
-            status: getMeetingStatus.status,
-        };
-
-        return res.status(200).send(getStatus);
-    } catch (err) {
-        return res.status(500).send({
-            message: "get a getMeetingStatushad an error",
-        });
-    }
-};
-
-
-
-exports.getMeetingStatusTest = async (req, res) => {
     console.log(`
 --------------------------------------------------
   API  : Get a meeting status
