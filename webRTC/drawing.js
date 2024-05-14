581f7e81fa81f95a976f860ee344ec18a9f98b98
limhokyun <lim@nsmarts.co.kr>
2024-05-14 10:57:07 +0900

    [feat] add video all clear function

--- webRTC/drawing.js
+++ webRTC/drawing.js
@@ -1,4 +1,5 @@
 module.exports = function (io, socket, app) {
+    // 비디오 위에 그림을 그렸음을 알려주는 소켓 신호
     socket.on('draw:video', async ({ room_id, data, target_id, user_id, meeting_id }) => {
 
         const dbModels = global.DB_MODELS;
@@ -13,5 +14,10 @@ module.exports = function (io, socket, app) {
 
 
         socket.to(room_id).emit('draw:video', { drawingEvent: data, socket_id: socket.id, target_id, user_id });
+    })
+
+    // 비디오 화면이 클리어 되었음을 알려주는 socket 신호
+    socket.on('draw:video_clear', async ({ room_id, meeting_id, target_id }) => {
+        socket.to(room_id).emit('draw:video_clear', { meeting_id, target_id })
     })
 }
