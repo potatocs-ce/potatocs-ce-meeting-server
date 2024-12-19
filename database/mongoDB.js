f0593c3324bb7863895e0971805491eeede1d1aa
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 16:18:56 +0900

    mongodb 코드 가독성 업데이트

--- database/mongoDB.js
+++ database/mongoDB.js
@@ -1,38 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 const mongoApp = {};
 mongoApp.appSetObjectId = function (app) {
-    app.set('ObjectId', mongoose.Types.ObjectId);
-    console.log('complete to set mongoose ObjectId');
-}
+	app.set("ObjectId", mongoose.Types.ObjectId);
+	console.log("complete to set mongoose ObjectId");
+};
 
-main().catch(err => console.error(err));
+main().catch((err) => console.error(err));
 
 async function main() {
-    await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }).then(() => {
-        createSchema();
-        console.log('Database Connected');
-    })
+	await mongoose
+		.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		.then(() => {
+			createSchema();
+			console.log("Database Connected");
+		});
 }
 
 function createSchema() {
-    const dbModels = {};
+	const dbModels = {};
 
+	dbModels.VideoDrawing = require("../models/video_drawing");
+	dbModels.DocDrawing = require("../models/doc_drawing");
+	dbModels.Meeting = require("../models/meeting_schema");
+	dbModels.Member = require("../models/member_schema");
+	dbModels.MeetingChat = require("../models/meetingChat_schema");
+	dbModels.Doc = require("../models/doc_schema");
+	dbModels.Survey = require("../models/survey_schema");
+	dbModels.Survey_Result = require("../models/survey_result_schema");
 
-    dbModels.VideoDrawing = require('../models/video_drawing');
-    dbModels.DocDrawing = require('../models/doc_drawing');
-    dbModels.Meeting = require('../models/meeting_schema');
-    dbModels.Member = require('../models/member_schema');
-    dbModels.MeetingChat = require('../models/meetingChat_schema');
-    dbModels.Doc = require('../models/doc_schema');
-    dbModels.Survey = require('../models/survey_schema');
-    dbModels.Survey_Result = require('../models/survey_result_schema')
-
-    global.DB_MODELS = dbModels;
+	global.DB_MODELS = dbModels;
 }
 
 module.exports = mongoApp;
