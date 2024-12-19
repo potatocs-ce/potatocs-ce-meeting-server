a5ec916b74f0047f9e11fca4c260d13a003bbc7b
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 17:01:34 +0900

    token 최적화

--- middlewares/libraries/token.js
+++ middlewares/libraries/token.js
@@ -1,25 +1,25 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // config에 따라 다르게..
 
 // JWT 토큰 생성
-exports.createToken = payload => {
-  const jwtOption = {
-    expiresIn: '1d'
-  };
-  return new Promise((resolve, reject) => {
-    jwt.sign(payload, process.env.JWT_SECRET, jwtOption, (error, token) => {
-      if (error) reject(error);
-      resolve(token);
-    });
-  });
+exports.createToken = (payload) => {
+	const jwtOption = {
+		expiresIn: "1d",
+	};
+	return new Promise((resolve, reject) => {
+		jwt.sign(payload, process.env.JWT_SECRET, jwtOption, (error, token) => {
+			if (error) reject(error);
+			resolve(token);
+		});
+	});
 };
 
 // JWT 토큰 검증
-exports.verifyToken = token => {
-  return new Promise((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
-      if (error) reject(error);
-      resolve(decoded);
-    });
-  });
+exports.verifyToken = (token) => {
+	return new Promise((resolve, reject) => {
+		jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+			if (error) reject(error);
+			resolve(decoded);
+		});
+	});
 };
