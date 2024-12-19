6ece25c9d4299352802ab9a17215d08b618ef24f
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 16:03:58 +0900

    index.js 코드 가독성 업데이트

--- routes/apim/v1/index.js
+++ routes/apim/v1/index.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const auth = require('./auth/auth_index');
-const meeting = require('./meeting/meeting_index');
-const doc = require('./doc/doc_index')
-const survey = require('./survey/survey_index');
-const { isAuthenticated } = require('../../../middlewares/auth')
-router.use('/auth', auth);
+const auth = require("./auth/auth_index");
+const meeting = require("./meeting/meeting_index");
+const doc = require("./doc/doc_index");
+const survey = require("./survey/survey_index");
+const { isAuthenticated } = require("../../../middlewares/auth");
+router.use("/auth", auth);
 
 router.use(isAuthenticated);
-router.use('/meeting', meeting);
-router.use('/doc', doc);
-router.use('/survey', survey);
+router.use("/meeting", meeting);
+router.use("/doc", doc);
+router.use("/survey", survey);
 
 module.exports = router;
