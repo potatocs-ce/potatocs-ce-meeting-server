a5af9045c297b45deb9496d9097daaf9a9438130
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 16:54:39 +0900

    video_drawing schema 업데이트

--- models/video_drawing.js
+++ models/video_drawing.js
@@ -1,38 +1,39 @@
-const mongoose = require('mongoose');
-const Meeting = require('./meeting_schema');
+const mongoose = require("mongoose");
+const Meeting = require("./meeting_schema");
 const Schema = mongoose.Schema;
 
-let videoDrawingSchema = new Schema({
+let videoDrawingSchema = new Schema(
+	{
+		// 어디에
+		meetingId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Meeting",
+		},
+		// 누구에게
+		targetId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Member",
+		},
+		// 누가
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Member",
+		},
 
-    // 어디에
-    meetingId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Meeting'
-    },
-    // 누구에게
-    targetId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Member'
-    },
-    // 누가
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Member'
-    },
+		//뭘
+		drawingEvent: {
+			type: Object,
+		},
+		// 스크린 공유 화면에 그렸는지 여부 판단
+		screen: {
+			type: Boolean,
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
 
-    //뭘
-    drawingEvent: {
-        type: Object
-    },
-    // 스크린 공유 화면에 그렸는지 여부 판단
-    screen: {
-        type: Boolean
-    }
-},
-    {
-        timestamps: true
-    })
-
-const VideoDrawing = mongoose.model('VideoDrawing', videoDrawingSchema);
+const VideoDrawing = mongoose.model("VideoDrawing", videoDrawingSchema);
 
 module.exports = VideoDrawing;
