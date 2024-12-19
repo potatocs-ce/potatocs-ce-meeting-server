2e2d25519aee843521dd6adffbd29139d5dcd8b6
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 16:20:13 +0900

    survey.index 구조 수정

--- routes/apim/v1/survey/survey_index.js
+++ routes/apim/v1/survey/survey_index.js
@@ -1,20 +1,19 @@
-const router = require('express').Router();
-const surveyController = require('./survey_controller');
+const router = require("express").Router();
+const surveyController = require("./survey_controller");
 
 // 설문지 등록
-router.post('/add', surveyController.addSurvey);
+router.post("/add", surveyController.addSurvey);
 // 설문 등록
-router.post('/:_id', surveyController.survey);
+router.post("/:_id", surveyController.survey);
 // 설문지 리스트 요청
-router.get('/meeting/:_meetingId', surveyController.getSurveys);
+router.get("/meeting/:_meetingId", surveyController.getSurveys);
 // 설문지 요청
-router.get('/:_id', surveyController.getSurvey);
+router.get("/:_id", surveyController.getSurvey);
 
-router.get('/result/:_id', surveyController.getSurveyResult);
+router.get("/result/:_id", surveyController.getSurveyResult);
 // 설문지 수정
-router.patch('/:_id', surveyController.editSurvey);
-// 설문 삭제 
-router.delete('/:_id', surveyController.deleteSurvey);
-
+router.patch("/:_id", surveyController.editSurvey);
+// 설문 삭제
+router.delete("/:_id", surveyController.deleteSurvey);
 
 module.exports = router;
