737e74b04ef7c2437a89476cccdd257dc96f9b8b
park9379 <109051896+park9379@users.noreply.github.com>
2024-12-19 16:17:31 +0900

    member_schema comparePassword 주석 추가

--- models/member_schema.js
+++ models/member_schema.js
@@ -1,96 +1,101 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const member_Schema = mongoose.Schema(
 	{
-		email: { 
+		email: {
 			type: String,
 			required: true,
 			unique: true,
 			lowercase: true,
 		},
-		password: { 
+		password: {
 			type: String,
-			required: true, 
+			required: true,
 			trim: true,
 		},
-		name: { 
+		name: {
 			type: String,
 			required: true,
 		},
-		profile_img: { 
+		profile_img: {
 			type: String,
-			default: ''
+			default: "",
 		},
-		mobile: { 
+		mobile: {
 			type: String,
-			default: ''
+			default: "",
 		},
-		department: { 
+		department: {
 			type: String,
-			default: '' 
+			default: "",
 		},
-		isManager: { 
-			type: Boolean, 
-			default: false
+		isManager: {
+			type: Boolean,
+			default: false,
 		},
-		position: { 
+		position: {
 			type: String,
-			default: ''
+			default: "",
 		},
-		annual_leave: { 
+		annual_leave: {
 			type: Number,
-			default: 0
+			default: 0,
 		},
-		sick_leave: { 
-			type: Number, 
-			default: 0
-		},
-		replacementday_leave: { 
+		sick_leave: {
 			type: Number,
-			default: 0
+			default: 0,
 		},
-		location: { 
+		replacementday_leave: {
+			type: Number,
+			default: 0,
+		},
+		location: {
 			type: String,
-			default: ''
+			default: "",
 		},
-		emp_start_date: { 
+		emp_start_date: {
 			type: Date,
-			default: null
+			default: null,
 		},
-		emp_end_date: { 
+		emp_end_date: {
 			type: Date,
-			default: null
+			default: null,
 		},
 	},
 	{
-		timestamps: true
+		timestamps: true,
 	}
 );
 
-member_Schema.pre('save', function(next){
-    var user = this;
+member_Schema.pre("save", function (next) {
+	var user = this;
 
-    bcrypt.genSalt(10, function(err, salt) {
-        if(err) return next(err)
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
 
-        bcrypt.hash(user.password, salt, function(err, hash){
-            if(err) return next(err)
-            user.password = hash
-            next()
-        })
-    });
-})
+		bcrypt.hash(user.password, salt, function (err, hash) {
+			if (err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
 
+// member_Schema의 메서드 comparePassword 정의
+// 사용자가 입력한 비밀번호와 저장된 해시 값(암호화된 비밀번호)을 비교하는 함수
 member_Schema.methods.comparePassword = function (password, hash) {
 	return new Promise((resolve, reject) => {
+		// bcrypt.compare를 사용하여 입력한 비밀번호와 해시 값 비교
 		bcrypt.compare(password, hash, (err, result) => {
+			// 에러가 발생하면 Promise를 reject로 처리
 			if (err) reject(err);
+			// 비교 결과가 성공적이면 Promise를 resolve로 처리
 			else resolve(result);
 		});
 	});
-}
+};
 
-const Member = mongoose.model('Member', member_Schema)
+const Member = mongoose.model("Member", member_Schema);
 
 module.exports = Member;
